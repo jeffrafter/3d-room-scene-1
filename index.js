c0268e003bd4883a9ac4af14f794a5fd4e5fa5dc
Jeff Rafter <jeffrafter@gmail.com>
2019-03-02 11:37:16 -0800

    Initial code

---
+++ index.js
@@ -0,0 +1,48 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+const script = "/Users/njero/Code/Examples/wiiuse/build/example/wiiuseexample";
+
+// Step 1 disconnect the wii remote
+// Step 2 hit up to turn on IR sensor
+// Step 3 sense it
+var spawn = require("child_process", [], { detached: true }).spawn;
+var child = spawn(script);
+child.stderr.on("data", function(data) {
+  const matches = data.toString().match(/IR z distance: (\d+\.?\d*)/);
+  if (matches) {
+    const z = Math.round(parseFloat(matches[1], 0));
+    console.log(z);
+    if (z > 0) io.emit("z", { z });
+  }
+});
+
+child.unref();
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/three.min.js", function(req, res) {
+  res.sendFile(__dirname + "/three.min.js");
+});
+
+io.on("connection", function(socket) {
+  console.log("a user connected");
+
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+});
+
+http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
+
+// let dist = 2;
+// setInterval(function() {
+//   dist += 1;
+//   if (dist > 40) dist = 2;
+//   io.emit("z", { z: dist });
+// }, 10);
