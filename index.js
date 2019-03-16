e1b8b48ea4b0bae1b5d072b4c7ab700f10497121
Tyler Davis <tee.s.davis@gmail.com>
2019-03-15 23:46:57 -0700

    Add point smoothing via averaging
    Interesting thing to play with is `this.averageOver`

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const zReadingToFeet = require("./lib/z-reading-to-feet");
+const { Averager } = require('./lib/averager')
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -17,12 +18,19 @@ app.get("/dat.gui.min.js", (req, res) => {
   res.sendFile(__dirname + "/vendor/dat.gui.min.js");
 });
 
+const averager = new Averager()
+
 io.on("connection", (socket) => {
   console.log("a user connected");
 
-  socket.on('wii-pos', message => {
-    console.log('got wii-pos')
-    io.emit('pos', message)
+  socket.on('wii-pos', position => {
+    // console.log(position)
+    const average = averager.averageWithPoint(position)
+    // console.log(average)
+
+    if (average) {
+      io.emit('pos', average)
+    }
   })
 
   socket.on("disconnect", () => {
